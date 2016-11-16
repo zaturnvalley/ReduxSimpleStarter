0a762c73cd4d3766554ffebc050ab9c617177400
zaturnvalley <zacharycmason@gmail.com>
2016-11-15 18:20:49 -0800

    app component appears on index.html, rendered

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
 // Step 1: Create a new component. This component
 // should produce some HTML
 const App = function() {
-  return <div>Hi! </div>;
+  return <div>Hi!</div>;
 }
 
 // Step 2: Take this component's generated HTML and
 // put it on the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
