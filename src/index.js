846c905ee7af82e9010fd71fac7253f5a58b9911
zaturnvalley <zacharycmason@gmail.com>
2016-11-16 14:53:47 -0800

    searchbar tag rendered in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from 'search_bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyARaSW1WXqD7H68CwzQcamkKqSPOTIBjLQ';
 
 // Step 1: Create a new component. This component
 // should produce some HTML
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Step 2: Take this component's generated HTML and
