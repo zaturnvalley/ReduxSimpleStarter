6f0d0743b29e1d66a2172f46958e6bc46fb76cd7
zaturnvalley <zacharycmason@gmail.com>
2016-11-15 18:31:22 -0800

    added components dir and comp js files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 
 // Step 1: Create a new component. This component
 // should produce some HTML
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return <div>Hi</div>;
 }
 
 // Step 2: Take this component's generated HTML and
