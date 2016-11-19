759024fa7abe9573df37c399011b1d89f9628166
zaturnvalley <zacharycmason@gmail.com>
2016-11-19 11:20:57 -0800

    changed app from funct component to class based component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyARaSW1WXqD7H68CwzQcamkKqSPOTIBjLQ';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
@@ -12,12 +11,14 @@ YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
 
 // Step 1: Create a new component. This component
 // should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extend Component {
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Step 2: Take this component's generated HTML and
