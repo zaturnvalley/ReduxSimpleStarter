82586f8c2b237fdb9181abb0ed340eaf8185bf89
zaturnvalley <zacharycmason@gmail.com>
2016-11-20 10:57:42 -0800

    video list item comp li video

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
     </ul>
   );
 };
