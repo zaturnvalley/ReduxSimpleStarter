f5304080259d83c2a231f549c53b358fc4bae037
zaturnvalley <zacharycmason@gmail.com>
2016-11-21 13:22:22 -0800

    iframe tag into videodetail

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -3,6 +3,9 @@ import React from 'react';
 const VideoDetail = (props) => {
   return (
     <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item"></iframe>
+      </div>
     </div>
   );
 };
