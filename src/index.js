b7d0bdb44608f6a1f935063d1a77972a14bbc64e
zaturnvalley <zacharycmason@gmail.com>
2016-11-22 12:25:37 -0800

    onSearchTermChange pulled into SearchBar comp

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ class App  extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
